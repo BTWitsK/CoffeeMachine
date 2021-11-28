1c88364497d6be99c2998eaefd0d8ef4423ac89e
BTWitsK <BTWitsK@Gmail.com>
2021-11-28 01:05:07 -0600

    finished stage 3

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -4,16 +4,38 @@ import java.util.Scanner;
 public class CoffeeMachine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Write how many cups of coffee you will need: ");
-        int coffeeCups = scanner.nextInt();
         final int water = 200;
         final int milk = 50;
-        final int coffeeBeans = 15;
+        final int beans= 15;
 
-        System.out.printf("For %d cups of coffee you will need:\n", coffeeCups);
-        System.out.printf("%d ml of water\n", water * coffeeCups);
-        System.out.printf("%d ml of milk\n", milk * coffeeCups);
-        System.out.printf("%d g of coffee beans\n", coffeeBeans * coffeeCups);
+        System.out.println("Write how many ml of water the coffee machine has: ");
+        int waterLeft = scanner.nextInt();
+
+        System.out.println("Write how many ml of milk the coffee machine has: ");
+        int milkLeft = scanner.nextInt();
+
+        System.out.println("Write how many grams of milk the coffee machine has: ");
+        int beansLeft = scanner.nextInt();
+
+        System.out.println("Write how many cups of coffee you will need: ");
+        int coffeeCups = scanner.nextInt();
+
+        int cups = Integer.min((waterLeft / water),Integer.min ((milkLeft / milk), (beansLeft / beans)));
+
+        if (Integer.compare(coffeeCups,cups) == 0) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (Integer.compare(coffeeCups, cups) < 0) {
+            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)\n", cups - coffeeCups);
+        } else {
+            System.out.printf("No, I can make only %d cup(s) of coffee", cups);
+        }
+
+
+
+
+
+
+
 
 
     }
